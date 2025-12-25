243af4f80a44f9d1b46fa744fccfce7f2a6f9e52
Shohanur Rahaman <shohan4556@gmail.com>
2025-12-25 22:14:43 +0600

    wip level integration

---
+++ src/game/BusLevelData.ts
@@ -0,0 +1,64 @@
+import { LevelData } from "./LevelData";
+
+export const LEVELS: LevelData[] = [
+    {
+        id: 1,
+        correctWord: "BERSIAR-SIAR",
+        meaningWord: "berjalan-jalan",
+        imageKey: "placeholder"
+    },
+    {
+        id: 2,
+        correctWord: "HINGGAP",
+        meaningWord: "terbang lalu berhenti di suatu tempat",
+        imageKey: "placeholder"
+    },
+    {
+        id: 3,
+        correctWord: "KENDERAAN",
+        meaningWord: "sesuatu yang digunakan oleh orang ramai untuk bergerak dari satu tempat ke tempat lain",
+        imageKey: "placeholder"
+    },
+    {
+        id: 4,
+        correctWord: "MENGHALAU",
+        meaningWord: "bergerak ke sesuatu tempat",
+        imageKey: "placeholder"
+    },
+    {
+        id: 5,
+        correctWord: "MENYESAL",
+        meaningWord: "rasa bersalah kerana melakukan sesuatu yang tidak baik",
+        imageKey: "placeholder"
+    },
+    {
+        id: 6,
+        correctWord: "MENYEWA",
+        meaningWord: "menggunakan sesuatu dengan membayar wang",
+        imageKey: "placeholder"
+    },
+    {
+        id: 7,
+        correctWord: "PANDUAN",
+        meaningWord: "arahan atau petunjuk yang membantu seseorang untuk mengetahui sesuatu",
+        imageKey: "placeholder"
+    },
+    {
+        id: 8,
+        correctWord: "TERBELIAK",
+        meaningWord: "membuka mata dengan besar",
+        imageKey: "placeholder"
+    },
+    {
+        id: 9,
+        correctWord: "TERPINGA-PINGA",
+        meaningWord: "hairan kerana tidak tahu perkara yang sedang berlaku",
+        imageKey: "placeholder"
+    },
+    {
+        id: 10,
+        correctWord: "TERUJA",
+        meaningWord: "sangat gembira atau bersemangat tentang sesuatu",
+        imageKey: "placeholder"
+    }
+];
---
+++ src/game/LevelData.ts
@@ -0,0 +1,7 @@
+export interface LevelData {
+    id: number;
+    imageKey: string;
+    correctWord: string;
+    meaningWord: string;
+    audioKey?: string;
+}
