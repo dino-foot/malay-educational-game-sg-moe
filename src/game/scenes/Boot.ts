4228cd9f72861f8daf63c5a369479f08b998f497
Shohanur Rahaman <shohan4556@gmail.com>
2025-12-25 13:48:54 +0600

    Update Boot.ts

--- src/game/scenes/Boot.ts
+++ src/game/scenes/Boot.ts
@@ -8,13 +8,7 @@ export class Boot extends Scene {
     preload() {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
-        // this.load.image('background', 'assets/bg.png');
         this.load.image('background', 'assets/menu/background.png');
-
-        // this.load.spineJson("boat-data", "assets/spine/boat.json");
-        // this.load.spineAtlas("boat-atlas", "assets/spine/boat.atlas");
-        // this.load.image('boat', 'assets/spine/boat.png');
-
     }
 
     create() {
