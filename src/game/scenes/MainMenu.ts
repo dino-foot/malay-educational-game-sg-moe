3cd4bda2b57c02ed24a4377244cf830502f02376
Shohanur Rahaman <shohan4556@gmail.com>
2026-01-06 18:40:20 +0600

    Update MainMenu.ts

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -4,7 +4,7 @@ import { Utils } from "./Utils";
 const menuButtons = [
     { key: "bas", scene: "BasScene" },
     { key: "kuasa", scene: "KuasaScene" },
-    { key: "kampung", scene: "KaysakScene" },
+    { key: "kampung", scene: "KayakScene" },
 ];
 export class MainMenu extends Scene {
     background: GameObjects.Image;
@@ -13,6 +13,8 @@ export class MainMenu extends Scene {
     centerXY: Phaser.Math.Vector2;
     kuasaBtn: GameObjects.Image;
     settingsBtn: GameObjects.Image;
+    watchLights: Phaser.GameObjects.Image[] = [];
+    watchBars: Phaser.GameObjects.Image[] = [];
 
     constructor() {
         super("MainMenu");
@@ -44,18 +46,39 @@ export class MainMenu extends Scene {
         Utils.AlignTopRight(this.settingsBtn, this.background, -20, -20);
         Utils.MakeButton(this, this.settingsBtn, () => {
             this.scene.launch("SettingsMenu");
-            // this.scene.launch('GameOver', {
-            //     currentScore: 99
-            // }); // SettingsMenu, GameOver
             this.scene.sendToBack();
             this.scene.pause();
         });
 
         const characterCointainer = this.add.container();
         const umar = this.add.image(x + 460, y + 70, "omar").setOrigin(0.5);
+        const glow = this.add.image(0, 0, "glow").setOrigin(0.5).setDepth(2);
+        const battery_off = this.add.image(0, 0, 'battery_off').setOrigin(0.5);
 
-        characterCointainer.add(umar);
-        characterCointainer.setDepth(1);
+        const greenLight = this.add.image(927, 205, 'green_light').setOrigin(0.5); // always on
+        const blueLight = this.add.image(952, 373, 'blue_light').setOrigin(0.5);
+        const yellowLight = this.add.image(1035, 193, 'yellow_light').setOrigin(0.5).setAngle(7);
+        const redLight = this.add.image(1057, 367, 'red_light').setOrigin(0.5);
+
+        const bar1 = this.add.image(968, 288, 'bar1').setOrigin(0.5);
+        const bar2 = this.add.image(990, 285, 'bar2').setOrigin(0.5);
+        const bar3 = this.add.image(1014, 281, 'bar3').setOrigin(0.5);
+
+        characterCointainer.setSize(300, 300)
+        characterCointainer.add([umar, battery_off, bar1, bar2, bar3, greenLight, blueLight, yellowLight, redLight]);
+        characterCointainer.setDepth(3);
+
+        Phaser.Display.Align.In.Center(glow, umar, -250, -320);
+        Phaser.Display.Align.In.Center(battery_off, umar, -270, -235);
+
+        this.watchLights = [blueLight, yellowLight, redLight];
+        this.watchBars = [bar1, bar2, bar3];
+
+        // Hide all initially
+        [...this.watchBars, ...this.watchLights].forEach(item => item.setVisible(false));
+
+        const completedLevels = this.registry.get('completedLevels') || 0;
+        this.updateProgress(completedLevels);
 
         //? debug start bas scene directly
         // this.scene.start("KuasaScene");
@@ -79,6 +102,13 @@ export class MainMenu extends Scene {
                 // this.scene.start(btn.scene);
             });
         });
+    }
+
+    private updateProgress(levelCompletedCount: number) {
+        for (let i = 0; i < levelCompletedCount; i++) {
+            if (this.watchLights[i]) this.watchLights[i].setVisible(true);
+            if (this.watchBars[i]) this.watchBars[i].setVisible(true);
+        }
     }
 
     btnHeight(key: string): number {
