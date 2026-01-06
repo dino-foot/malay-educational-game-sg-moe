1d5b74c96ed850acfac7663d9e799bb64c88e6e3
Shohanur Rahaman <shohan4556@gmail.com>
2026-01-06 22:48:44 +0600

    updated level data

--- src/game/KuasaLevelData.ts
+++ src/game/KuasaLevelData.ts
@@ -3,72 +3,62 @@ import { TrainLevelData } from "./LevelData";
 export const KUASA_LEVEL_DATA: TrainLevelData[] = [
     {
         id: 1,
-        imageKey: "placeholder",
-        trainWord: "bersiar-siar",
+        correctWord: "bersiar-siar",
         hintWord: "berjalan-jalan",
-        hintSentence: "Umar dan keluarganya ______ di Sentosa."
+        hintSentence: "Umar dan keluarganya ______ di Sentosa.",
     },
     {
         id: 2,
-        imageKey: "placeholder",
-        trainWord: "hinggap",
+        correctWord: "hinggap",
         hintWord: "terbang lalu berhenti di suatu tempat",
-        hintSentence: "Burung itu ______ di dahan pokok."
+        hintSentence: "Burung itu ______ di dahan pokok.",
     },
     {
         id: 3,
-        imageKey: "placeholder",
-        trainWord: "kenderaan",
+        correctWord: "kenderaan",
         hintWord: "sesuatu yang digunakan oleh orang ramai untuk bergerak dari satu tempat ke tempat lain",
-        hintSentence: "Kereta api MRT ialah ______ yang selalu digunakan oleh rakyat Singapura."
+        hintSentence: "Kereta api MRT ialah ______ yang selalu digunakan oleh rakyat Singapura.",
     },
     {
         id: 4,
-        imageKey: "placeholder",
-        trainWord: "menghalau",
+        correctWord: "menghalau",
         hintWord: "bergerak ke sesuatu tempat",
-        hintSentence: "Ibu Tanggang ______ angsa yang mematuk Tanggang."
+        hintSentence: "Ibu Tanggang ______ angsa yang mematuk Tanggang.",
     },
     {
         id: 5,
-        imageKey: "placeholder",
-        trainWord: "menyesal",
+        correctWord: "menyesal",
         hintWord: "rasa bersalah kerana melakukan sesuatu yang tidak baik",
-        hintSentence: "Tanggang ______ kerana bersikap biadab terhadap ibunya."
+        hintSentence: "Tanggang ______ kerana bersikap biadab terhadap ibunya.",
     },
     {
         id: 6,
-        imageKey: "placeholder",
-        trainWord: "menyewa",
+        correctWord: "menyewa",
         hintWord: "menggunakan sesuatu dengan membayar wang",
-        hintSentence: "Ayah ______ kayak untuk kami sekeluarga."
+        hintSentence: "Ayah ______ kayak untuk kami sekeluarga.",
     },
     {
         id: 7,
-        imageKey: "placeholder",
-        trainWord: "panduan",
+        correctWord: "panduan",
         hintWord: "arahan atau petunjuk yang membantu seseorang untuk mengetahui sesuatu",
-        hintSentence: "Abang membaca ______ untuk memasang kereta mainan yang baru dibeli."
+        hintSentence: "Abang membaca ______ untuk memasang kereta mainan yang baru dibeli.",
     },
     {
         id: 8,
-        imageKey: "placeholder",
-        trainWord: "terbelalak",
+        correctWord: "terbelalak",
         hintWord: "membuka mata dengan besar",
-        hintSentence: "Mata Umar ______ apabila ternampak Tanggang."
+        hintSentence: "Mata Umar ______ apabila ternampak Tanggang.",
     },
     {
         id: 9,
-        imageKey: "placeholder",
-        trainWord: "terpinga-pinga",
+        correctWord: "terpinga-pinga",
         hintWord: "hairan kerana tidak tahu perkara yang sedang berlaku",
-        hintSentence: "Saya ______ apabila terdengar bunyi letupan."
+        hintSentence: "Saya ______ apabila terdengar bunyi letupan.",
     },
     {
         id: 10,
-        imageKey: "placeholder",
-        trainWord: "teruja",
+        correctWord: "teruja",
         hintWord: "sangat gembira atau bersemangat tentang sesuatu",
-        hintSentence: "Adik ______ menaiki bas dua tingkat untuk kali pertama."
-    }
+        hintSentence: "Adik ______ menaiki bas dua tingkat untuk kali pertama.",
+    },
 ];
