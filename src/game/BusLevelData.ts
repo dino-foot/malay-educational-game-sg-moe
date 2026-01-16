28d46875b03002dbccb44a3ec41bc167e02dd10c
Shohanur Rahaman <shohan4556@gmail.com>
2026-01-16 20:45:55 +0600

    new arts and typo fixed

--- src/game/BusLevelData.ts
+++ src/game/BusLevelData.ts
@@ -1,64 +1,66 @@
 import { LevelData } from "./LevelData";
 
+// todo dont use image use hint sentence for id 1 and 10
+
 export const BUS_LEVELS_DATA: LevelData[] = [
     {
         id: 1,
         correctWord: "BERSIAR-SIAR",
-        hintSentence: "berjalan-jalan",
+        hintSentence: "Berjalan-jalan.",
         imageKey: null, //! dont use image for this question use sentenece
     },
     {
         id: 2,
         correctWord: "HINGGAP",
-        hintSentence: "terbang lalu berhenti di suatu tempat",
+        hintSentence: "Terbang lalu berhenti di suatu tempat.",
         imageKey: "hinggap"
     },
     {
         id: 3,
         correctWord: "KENDERAAN",
-        hintSentence: "sesuatu yang digunakan oleh orang ramai untuk bergerak dari satu tempat ke tempat lain",
+        hintSentence: "Sesuatu yang digunakan oleh orang ramai untuk bergerak dari satu tempat ke tempat lain.",
         imageKey: "kenderaan"
     },
     {
         id: 4,
         correctWord: "MENGHALAU",
-        hintSentence: "bergerak ke sesuatu tempat",
+        hintSentence: "Bergerak ke sesuatu tempat.",
         imageKey: "menghalau"
     },
     {
         id: 5,
         correctWord: "MENYESAL",
-        hintSentence: "rasa bersalah kerana melakukan sesuatu yang tidak baik",
+        hintSentence: "Rasa bersalah kerana melakukan sesuatu yang tidak baik.",
         imageKey: "menyesal"
     },
     {
         id: 6,
         correctWord: "MENYEWA",
-        hintSentence: "menggunakan sesuatu dengan membayar wang",
+        hintSentence: "Menggunakan sesuatu dengan membayar wang.",
         imageKey: "menyewa"
     },
     {
         id: 7,
         correctWord: "PANDUAN",
-        hintSentence: "arahan atau petunjuk yang membantu seseorang untuk mengetahui sesuatu",
+        hintSentence: "Arahan atau petunjuk yang membantu seseorang untuk mengetahui sesuatu.",
         imageKey: "panduan"
     },
     {
         id: 8,
         correctWord: "TERBELIAK",
-        hintSentence: "membuka mata dengan besar",
+        hintSentence: "Membuka mata dengan besar.",
         imageKey: "terbeliak"
     },
     {
         id: 9,
         correctWord: "TERPINGA-PINGA",
-        hintSentence: "hairan kerana tidak tahu perkara yang sedang berlaku",
+        hintSentence: "Hairan kerana tidak tahu perkara yang sedang berlaku.",
         imageKey: "terpinga-pinga"
     },
     {
         id: 10,
         correctWord: "TERUJA",
-        hintSentence: "sangat gembira atau bersemangat tentang sesuatu",
+        hintSentence: "Sangat gembira atau bersemangat tentang sesuatu.",
         imageKey: "teruja"
     }
 ];
