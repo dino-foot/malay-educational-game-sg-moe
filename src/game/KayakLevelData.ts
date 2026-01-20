47e159f324684fbfa09a03703d5848b2413f2424
Shohanur Rahaman <shohan4556@gmail.com>
2026-01-20 15:12:13 +0600

    Update KayakLevelData.ts

--- src/game/KayakLevelData.ts
+++ src/game/KayakLevelData.ts
@@ -3,62 +3,62 @@ import { KayakLevelData } from "./LevelData";
 export const KAYAK_LEVEL_DATA: KayakLevelData[] = [
     {
         id: 1,
-        correctWord: "Bersiar-siar",
+        correctWord: "bersiar-siar",
         fillinTheGap: "Umar dan keluarganya ___________________ di Sentosa.",
-        hintSentence: "Pada hari Ahad, keluarga kami bersiar-siar di taman sambil menikmati udara yang segar."
+        hintSentence: "Pada hari Ahad, keluarga kami ______________ di taman sambil menikmati udara yang segar."
     },
     {
         id: 2,
-        correctWord: "Hinggap",
+        correctWord: "hinggap",
         fillinTheGap: "Burung itu ___________________ di dahan pokok.",
-        hintSentence: "Seekor rama-rama yang cantik hinggap di atas bunga ros di taman sekolah kami."
+        hintSentence: "Seekor rama-rama yang cantik ______________ di atas bunga ros di taman sekolah kami."
     },
     {
         id: 3,
-        correctWord: "Kenderaan",
+        correctWord: "kenderaan",
         fillinTheGap: "Kereta api MRT ialah ___________________ yang selalu digunakan oleh rakyat Singapura.",
-        hintSentence: "Jalan raya itu sesak dengan pelbagai kenderaan seperti kereta, bas dan motosikal."
+        hintSentence: "Jalan raya itu sesak dengan pelbagai ______________ seperti kereta, bas dan motosikal."
     },
     {
         id: 4,
-        correctWord: "Menghalau",
+        correctWord: "menghalau",
         fillinTheGap: "Ibu Tanggang ___________________ angsa yang mematuk Tanggang.",
-        hintSentence: "Ibu menghalau lalat yang hinggap di atas meja makan dengan menggunakan kipas."
+        hintSentence: "Ibu ______________ lalat yang hinggap di atas meja makan dengan menggunakan kipas."
     },
     {
         id: 5,
-        correctWord: "Menyesal",
+        correctWord: "menyesal",
         fillinTheGap: "Tanggang ___________________ kerana bersikap biadab terhadap ibunya.",
-        hintSentence: "Adik menyesal lalu meminta maaf kerana tidak mendengar nasihat ibu."
+        hintSentence: "Adik ______________ lalu meminta maaf kerana tidak mendengar nasihat ibu."
     },
     {
         id: 6,
-        correctWord: "Menyewa",
+        correctWord: "menyewa",
         fillinTheGap: "Ayah ___________________ kayak untuk kami sekeluarga.",
-        hintSentence: "Ayah menyewa sebuah kereta untuk kami pergi bercuti ke Pulau Pinang."
+        hintSentence: "Ayah ______________ sebuah kereta untuk kami pergi bercuti ke Pulau Pinang."
     },
     {
         id: 7,
-        correctWord: "Panduan",
+        correctWord: "panduan",
         fillinTheGap: "Abang membaca ___________________ untuk memasang kereta mainan yang baru dibeli.",
-        hintSentence: "Cikgu memberikan panduan yang jelas supaya kami dapat menyiapkan projek sains dengan betul."
+        hintSentence: "Cikgu memberikan ______________ yang jelas supaya kami dapat menyiapkan projek sains dengan betul."
     },
     {
         id: 8,
-        correctWord: "Terbeliak",
+        correctWord: "terbeliak",
         fillinTheGap: "Mata Umar ___________________ apabila ternampak Tanggang.",
-        hintSentence: "Mata Ali terbelalak apabila dia melihat kek hari lahir yang besar dan cantik itu."
+        hintSentence: "Mata Ali ______________ apabila dia melihat kek hari lahir yang besar dan cantik itu."
     },
     {
         id: 9,
-        correctWord: "Terpinga-pinga",
+        correctWord: "terpinga-pinga",
         fillinTheGap: "Saya ___________________ apabila terdengar bunyi letupan.",
-        hintSentence: "Kakak terpinga-pinga melihat persembahan silap mata yang sangat mengagumkan."
+        hintSentence: "Kakak ______________ melihat persembahan silap mata yang sangat mengagumkan."
     },
     {
         id: 10,
-        correctWord: "Teruja",
+        correctWord: "teruja",
         fillinTheGap: "Adik ___________________ menaiki bas dua tingkat untuk kali pertama.",
-        hintSentence: "Kami berasa teruja kerana akan pergi ke Taman Haiwan bersama-sama esok."
+        hintSentence: "Kami berasa ______________ kerana akan pergi ke Taman Haiwan bersama-sama esok."
     }
 ];
