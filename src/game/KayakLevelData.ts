df50f27aec4e6f3b89077249abfb389f15964276
Shohanur Rahaman <shohan4556@gmail.com>
2026-02-12 20:37:09 +0600

    Update KayakLevelData.ts

--- src/game/KayakLevelData.ts
+++ src/game/KayakLevelData.ts
@@ -41,7 +41,7 @@ export const KAYAK_LEVEL_DATA: KayakLevelData[] = [
         id: 7,
         correctWord: "panduan",
         fillinTheGap: "Abang membaca ___________________ untuk memasang kereta mainan yang baru dibelinya.",
-        hintSentence: "Cikgu Siti memberikan ___________________ yang jelas supaya kami dapat menyiapkan projek sains dengan betul."
+        hintSentence: "Cikgu Siti memberikan ___________________ untuk murid menyiapkan projek Sains."
     },
     {
         id: 8,
