c11ea150e20e0681504cea83bdfe484da4b55fbe
Shohanur Rahaman <shohan4556@gmail.com>
2026-02-12 20:27:35 +0600

    fixed

--- src/game/BusLevelData.ts
+++ src/game/BusLevelData.ts
@@ -6,61 +6,61 @@ export const BUS_LEVELS_DATA: LevelData[] = [
     {
         id: 1,
         correctWord: "BERSIAR-SIAR",
-        hintSentence: "Berjalan-jalan.",
+        hintSentence: "berjalan-jalan",
         imageKey: null, //! dont use image for this question use sentenece
     },
     {
         id: 2,
         correctWord: "HINGGAP",
-        hintSentence: "Terbang lalu berhenti di suatu tempat.",
+        hintSentence: "terbang lalu berhenti di suatu tempat",
         imageKey: "hinggap"
     },
     {
         id: 3,
         correctWord: "KENDERAAN",
-        hintSentence: "Sesuatu yang digunakan oleh orang ramai untuk bergerak dari satu tempat ke tempat lain.",
+        hintSentence: "sesuatu yang digunakan oleh orang ramai untuk bergerak dari satu tempat ke tempat lain",
         imageKey: "kenderaan"
     },
     {
         id: 4,
         correctWord: "MENGHALAU",
-        hintSentence: "Bergerak ke sesuatu tempat.",
+        hintSentence: "bergerak ke sesuatu tempat",
         imageKey: "menghalau"
     },
     {
         id: 5,
         correctWord: "MENYESAL",
-        hintSentence: "Rasa bersalah kerana melakukan sesuatu yang tidak baik.",
+        hintSentence: "rasa bersalah kerana melakukan sesuatu yang tidak baik",
         imageKey: "menyesal"
     },
     {
         id: 6,
         correctWord: "MENYEWA",
-        hintSentence: "Menggunakan sesuatu dengan membayar wang.",
+        hintSentence: "menggunakan sesuatu dengan membayar wang",
         imageKey: "menyewa"
     },
     {
         id: 7,
         correctWord: "PANDUAN",
-        hintSentence: "Arahan atau petunjuk yang membantu seseorang untuk mengetahui sesuatu.",
+        hintSentence: "arahan atau petunjuk yang membantu seseorang untuk mengetahui sesuatu",
         imageKey: "panduan"
     },
     {
         id: 8,
         correctWord: "TERBELIAK",
-        hintSentence: "Membuka mata dengan besar.",
+        hintSentence: "membuka mata dengan besar",
         imageKey: "terbeliak"
     },
     {
         id: 9,
         correctWord: "TERPINGA-PINGA",
-        hintSentence: "Hairan kerana tidak tahu perkara yang sedang berlaku.",
+        hintSentence: "hairan kerana tidak tahu perkara yang sedang berlaku",
         imageKey: "terpinga-pinga"
     },
     {
         id: 10,
         correctWord: "TERUJA",
-        hintSentence: "Sangat gembira atau bersemangat tentang sesuatu.",
+        hintSentence: "sangat gembira atau bersemangat tentang sesuatu",
         imageKey: null //! dont use image for this question use sentenece
     }
 ];
